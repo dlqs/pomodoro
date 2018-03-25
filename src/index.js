feff75d38fa1a09d958bb8f4a7764d052975813b
dlqs <d-lee@comp.nus.edu.sg>
2018-03-25 16:01:48 +0800

    Add static version
     - Add TimeDisplay, ModeController, ModeSelector
     - Display current unix time

--- src/index.js
+++ src/index.js
@@ -2,9 +2,61 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class PomodoroContainer extends React.Component {
+class ModeSelector extends React.Component {
   render() {
-    return <h1>Pomodoro!</h1>;
+    return (
+      <div>
+        <button type='button'> Pomodoro </button>
+        <button type='button'> Short break </button>
+        <button type='button'> Long break </button>
+      </div>
+    )
   }
 }
+
+class TimeDisplay extends React.Component {
+  render() {
+    return (
+      <h2> {this.props.time} </h2>
+    );
+  }
+}
+
+class ModeController extends React.Component {
+  render() {
+    return (
+      <div>
+        <button type='button'> Start </button>
+        <button type='button'> Stop </button>
+        <button type='button'> Reset </button>
+      </div>
+
+    )
+  }
+}
+
+// contains timer parts
+class TimerContainer extends React.Component {
+  render() {
+    return (
+      <div>
+        <ModeSelector />
+        <TimeDisplay time={Date.now()}/>
+        <ModeController />
+      </div>
+    )
+  }
+}
+// Contains the entirety of this example
+class PomodoroContainer extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Pomodoro!</h1>;
+        <TimerContainer />
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(<PomodoroContainer />, document.getElementById('container'))
