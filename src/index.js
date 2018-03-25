1dfaebbb7b8143a7a198e70447ccd349d2886104
dlqs <d-lee@comp.nus.edu.sg>
2018-03-26 01:04:28 +0800

    Change structure of components
     - Add working counter

--- src/index.js
+++ src/index.js
@@ -7,22 +7,22 @@ class ModeSelector extends React.Component {
     return (
       <div>
         <button type='button'> Pomodoro </button>
-        <button type='button'> Short break </button>
-        <button type='button'> Long break </button>
+        <button type='button'> Short </button>
+        <button type='button'> Long </button>
       </div>
     )
   }
 }
 
-class TimeDisplay extends React.Component {
-  render() {
-    return (
-      <h2> {this.props.time} </h2>
-    );
-  }
-}
+//class TimeDisplayer extends React.Component {
+//  render() {
+//    return (
+//      <h2> {setInterval(count, 1000)} </h2>
+//    );
+//  }
+//}
 
-class ModeController extends React.Component {
+class TimeController extends React.Component {
   render() {
     return (
       <div>
@@ -35,24 +35,50 @@ class ModeController extends React.Component {
   }
 }
 
-// contains timer parts
-class TimerContainer extends React.Component {
+class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {counter: 0};
+  }
+  componentDidMount() {
+    this.timer = setInterval(() => this.tick(), 1000)
+  }
+  // why is props taken in as an argument if its not needed?
+  tick() {
+    this.setState((prevState, props) => ({
+      counter: prevState.counter + 1
+    }))
+  }
   render() {
     return (
       <div>
-        <ModeSelector />
-        <TimeDisplay time={Date.now()}/>
-        <ModeController />
+        {this.state.counter}
+        <TimeController />
       </div>
     )
   }
 }
-// Contains the entirety of this example
+
+class TimerContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {duration: "900"};
+  }
+
+  render() {
+    return (
+      <div>
+        <ModeSelector />
+        <Timer duration={this.state.duration} />
+      </div>
+    );
+  }
+}
 class PomodoroContainer extends React.Component {
   render() {
     return (
       <div>
-        <h1>Pomodoro!</h1>;
+        <h1>Pomodoro!</h1>
         <TimerContainer />
       </div>
     );
